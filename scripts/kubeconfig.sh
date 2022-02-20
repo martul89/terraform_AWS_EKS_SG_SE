b80255b1bf76642591d6163eeed0f752a87acdb3
martul89 <martul89@gmail.com>
2022-02-20 09:32:45 +0000

    first commit

--- scripts/kubeconfig.sh
+++ scripts/kubeconfig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir /home/martul/.kube/
-terraform output -raw kubeconfig > /home/martul/.kube/config
+mkdir ~/.kube/
+terraform output -raw kubeconfig > ~/.kube/config
 aws configure set aws_access_key_id ${access_key}
 aws configure set aws_secret_access_key ${secret_key}
 aws configure set region ${region}
